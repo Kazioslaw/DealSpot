05e58b3a9589e78cb7ed0d6068bd083963f457f2
Kazioslaw <Kamil_d13@wp.pl>
2025-02-11 02:55:20 +0100

    Created Migration

---
+++ DealSpot/Data/Migrations/20250211015416_MakeNLastrejectedPriceTimeNullableAndAddNegotiationPriceProposedTime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DealSpot.Data.migrations
+{
+    /// <inheritdoc />
+    public partial class MakeNLastrejectedPriceTimeNullableAndAddNegotiationPriceProposedTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastRejectedPriceTime",
+                table: "Negotiation",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NegotiationPriceProposedTime",
+                table: "Negotiation",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NegotiationPriceProposedTime",
+                table: "Negotiation");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastRejectedPriceTime",
+                table: "Negotiation",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
